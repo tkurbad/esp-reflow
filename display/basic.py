c44faf4d675267047cccbf21210aeb4171d47a50
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2019-02-27 18:34:40 +0100

    Added (untested) code to display heater PWM duty settings

--- display/basic.py
+++ display/basic.py
@@ -1,10 +1,6 @@
 ## ESP32 Reflow Oven
 #
-# Temperature Read and Display Thread
-
-import _thread
-
-from time import sleep
+# Display Class that Does All the Work on the TFT
 
 from ili9341 import ILI9341, color565
 from ili9341.fonts import tt14, tt24
@@ -33,6 +29,11 @@ class Display(ILI9341):
                             config.DISPLAY_TOP_BAR_HEIGHT,
                             color = config.DISPLAY_STATUS_BG_COLOR)
         self.fill_rectangle(0,
+                            config.DISPLAY_HEATER_DELIM_Y,
+                            config.DISPLAY_WIDTH,
+                            1,
+                            color = config.DISPLAY_STATUS_BG_COLOR)
+        self.fill_rectangle(0,
                             config.DISPLAY_LOW_BAR_Y1,
                             config.DISPLAY_WIDTH,
                             config.DISPLAY_LOW_BAR_HEIGHT,
@@ -48,6 +49,12 @@ class Display(ILI9341):
                                       config.DISPLAY_TOP_BAR_TEXT_Y)
         self.set_font(tt24)
         self.tc_x = dict()
+        for index in range(0:2):
+            self.heater_x[index] = self.chars(
+                                    config.HEATER_LABEL[index],
+                                    config.DISPLAY_HEATER_TEXT_X[index],
+                                    config.DISPLAY_HEATER_TEXT_Y
+                                    )
         for tc_name in [config.THERMOCOUPLE_NAME1,
                         config.THERMOCOUPLE_NAME2,
                         config.THERMOCOUPLE_NAME3,
@@ -85,3 +92,15 @@ class Display(ILI9341):
         self.chars('%.2f ' % max(internal_temps),
                    self.tc_x[config.THERMOCOUPLE_NAME4],
                    config.DISPLAY_LOW_BAR_TEXT_Y[config.THERMOCOUPLE_NAME4])
+
+    def show_heaters(self, heaters_pwm):
+        if not self.prepared:
+            return
+        if len(heaters_pwm) != 2:
+            return
+        self.set_color(config.DISPLAY_HEATER_FG_COLOR,
+                       config.DISPLAY_HEATER_BG_COLOR)
+        self.chars('%.1f' % heaters_pwm[0], self.heater_x[0],
+                   config.DISPLAY_HEATER_TEXT_Y)
+        self.chars('%.1f' % heaters_pwm[1], self.heater_x[1],
+                   config.DISPLAY_HEATER_TEXT_Y)
