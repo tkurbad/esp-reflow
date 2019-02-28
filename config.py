734ab13f0513731c3f0370e9b3b34227d10bdde1
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2019-02-28 13:56:57 +0100

    Added Menu config / various config settings

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from micropython import const
 
 from ili9341 import color565
+from ili9341.fonts import tt14, tt24
 from hwspi.constants import HSPI, VSPI
 
 # Thermocouple Settings
@@ -19,6 +20,7 @@ THERMOCOUPLE_LABEL      = {
     THERMOCOUPLE_NAME3: 'T3 ',
     THERMOCOUPLE_NAME4: 'Int '
 }
+NUM_THERMOCOUPLES       = const(3)
 
 # SD Card Settings
 SDCARD_BUSID            = HSPI
@@ -34,14 +36,51 @@ DISPLAY_CS_PIN          = const(22)
 DISPLAY_HEIGHT          = const(320)
 DISPLAY_WIDTH           = const(240)
 
-# Display Color Settings
+# Display Basic Color / Font Settings
+DISPLAY_LABEL_FONT      = tt14
+DISPLAY_LABEL_X         = const(10)
 DISPLAY_BG_COLOR        = color565(0, 0, 0)
 DISPLAY_STATUS_BG_COLOR = color565(200, 200, 200)
 DISPLAY_STATUS_FG_COLOR = DISPLAY_BG_COLOR
-DISPLAY_HEATER_BG_COLOR = DISPLAY_BG_COLOR
-DISPLAY_HEATER_FG_COLOR = color565(220, 220, 220)
 
-# Display Status Bar Coordinates
+# Buzzer
+BUZZER_PIN              = const(0)
+BUZZER_PWM_TIMER        = const(1)
+BUZZER_VOLUME           = 30.0
+BUZZER_NOTE_CSHARP      = dict()
+_CSHARP_BASE            = 17.323914
+for octave in range(0, 12):
+    BUZZER_NOTE_CSHARP[octave] = const(int(round(_CSHARP_BASE)))
+    _CSHARP_BASE        *= 2
+
+# Heaters
+HEATER_NAME_TOP         = 'top'
+HEATER_NAME_BOTTOM      = 'bottom'
+HEATER_LABEL            = {
+    HEATER_NAME_TOP: 'Top ',
+    HEATER_NAME_BOTTOM: 'Bot '
+}
+HEATER_BOTTOM_PIN       = const(27)
+HEATER_TOP_PIN          = const(26)
+HEATER_PWM_FREQ         = const(5)
+HEATER_PWM_TIMER        = const(2)
+NUM_HEATERS             = const(2)
+
+# Fan
+FAN_PIN                 = const(32)
+FAN_PWM_FREQ            = const(500)
+FAN_PWM_TIMER           = const(3)
+
+# Light
+LIGHT_PIN               = const(25)
+
+# Status Bar Display Coordinates
+DISPLAY_TOP_BAR_FONT    = tt14
+DISPLAY_LOW_BAR_FONT    = tt24
+DISPLAY_TOP_BAR_LABEL   = 'Heaters'
+DISPLAY_TOP_BAR_LABEL_Y = const(230)
+DISPLAY_LOW_BAR_LABEL   = 'Thermocouples'
+DISPLAY_LOW_BAR_LABEL_Y = const(250)
 DISPLAY_TOP_BAR_Y       = const(0)
 DISPLAY_LOW_BAR_Y1      = const(260)
 DISPLAY_LOW_BAR_Y2      = const(290)
@@ -62,32 +101,25 @@ DISPLAY_LOW_BAR_TEXT_Y  = {
     THERMOCOUPLE_NAME4: const(292)
 }
 
-# Display Heater Bar Coordinates
+# Heater Bar Display Coordinates
+DISPLAY_HEATER_FONT     = tt24
+DISPLAY_HEATER_BG_COLOR = DISPLAY_BG_COLOR
+DISPLAY_HEATER_FG_COLOR = color565(220, 220, 220)
 DISPLAY_HEATER_DELIM_Y  = const(230)
-DISPLAY_HEATER_TEXT_X   = [const(10), const(125)]
+DISPLAY_HEATER_TEXT_X   = {
+    HEATER_NAME_TOP: const(10),
+    HEATER_NAME_BOTTOM: const(125)
+}
 DISPLAY_HEATER_TEXT_Y   = const(232)
 
-# Buzzer
-BUZZER_PIN              = const(0)
-BUZZER_PWM_TIMER        = const(1)
-BUZZER_VOLUME           = 30.0
-BUZZER_NOTE_CSHARP      = dict()
-_CSHARP_BASE            = 17.323914
-for octave in range(0, 12):
-    BUZZER_NOTE_CSHARP[octave] = const(int(round(_CSHARP_BASE)))
-    _CSHARP_BASE        *= 2
-
-# Heaters
-HEATER_LABEL           = ['Top', 'Bot']
-HEATER_BOTTOM_PIN       = const(27)
-HEATER_TOP_PIN          = const(26)
-HEATER_PWM_FREQ         = const(5)
-HEATER_PWM_TIMER        = const(2)
-
-# Fan
-FAN_PIN                 = const(32)
-FAN_PWM_FREQ            = const(500)
-FAN_PWM_TIMER           = const(3)
-
-# Light
-LIGHT_PIN               = const(25)
+# Menu Display Settings
+MENU_FONT               = tt14
+MENU_START_X            = const(5)
+MENU_START_Y            = const(25)
+MENU_WIDTH              = const(230)
+MENU_HEIGHT             = const(180)
+MENU_BG_COLOR           = color565(0, 0, 20)
+MENU_ACTIVE_BG_COLOR    = color565(0, 236, 234)
+MENU_ACTIVE_ITEM_COLOR  = color565(255, 240, 0)
+MENU_INACTIVE_BG_COLOR  = color565(0, 0, 20)
+MENU_INACTIVE_ITEM_COLOR = color565(255, 240, 0)
