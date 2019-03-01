5bcecdd4f241e478909d76b921831a9d448ff849
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2019-03-01 14:21:27 +0100

    Greatly simplify menu code

--- reflow/menu.py
+++ reflow/menu.py
@@ -1,15 +1,12 @@
+import gc
+
 import config
 
 class Menu:
     active = 0
     last_active = 0
-    num_items = 0
 
-    def __init__(self, menuitems, display, rotary,
-                 x = None, y = None, w = None, h = None,
-                 bg_color = None,
-                 active_bg_color = None, active_item_color = None, 
-                 inactive_bg_color = None, inactive_item_color = None):
+    def __init__(self, menuitems, display, rotary, lock = None):
         """ Initialize Menu Area.
 
             Parameters:
@@ -17,55 +14,50 @@ class Menu:
                 menuitems           - ???
                 display             - A display.basic.Display Object
                 rotary              - A reflow.device.Rotary Object
-                x                   - X Start Coordinate of Menu on
-                                      Display in Pixels
-                y                   - Y Start Coordinate of Menu on
-                                      Display in Pixels
-                w                   - Width of Menu on Display in Pixels
-                h                   - Height of Menu on Display in
-                                      Pixels
-                bg_color            - Background Color of Menu Area
-                active_bg_color     - Background Color of Active Menu
-                                      Item
-                active_item_color   - Text Color of Active Menu Item
-                inactive_bg_color   - Background Color of Inactive Menu
-                                      Items
-                inactive_item_color - Text Color of Inactive Menu Items
-
         """
         self.menuitems = menuitems
         self.num_items = len(menuitems)
-        self.rotary = rotary
         self.display = display
-        self.x = x
-        if x is None:
-            self.x = config.MENU_START_X
-        self.y = y
-        if x is None:
-            self.x = config.MENU_START_Y
-        self.w = w
-        if w is None:
-            self.w = config.MENU_WIDTH
-        self.h = h
-        if h is None:
-            self.h = config.MENU_HEIGHT
-        self.bg_color = bg_color
-        if bg_color is None:
-            self.bg_color = config.MENU_BG_COLOR
-        self.active_bg_color = active_bg_color
-        if active_bg_color is None:
-            self.active_bg_color = config.MENU_ACTIVE_BG_COLOR
-        self.active_item_color = active_item_color
-        if active_item_color is None:
-            self.active_item_color = config.MENU_ACTIVE_ITEM_COLOR
-        self.inactive_bg_color = inactive_bg_color
-        if inactive_bg_color is None:
-            self.inactive_bg_color = config.MENU_INACTIVE_BG_COLOR
-        self.inactive_item_color = inactive_item_color
-        if inactive_item_color is None:
-            self.inactive_item_color = config.MENU_INACTIVE_ITEM_COLOR
-        self.clear_menu_area()
+        self.rotary = rotary
+        self.lock = lock
+        self.clear()
+        gc.collect()
 
-    def clear_menu_area():
-        self.display.fill_rectangle(self.x, self.y, self.w, self.h,
-                                    color = self.bg_color)
+    def _clear(self):
+        self.display.fill_rectangle(config.MENU_START_X,
+                                    config.MENU_START_Y,
+                                    config.MENU_WIDTH,
+                                    config.MENU_HEIGHT,
+                                    color = config.MENU_BG_COLOR)
+
+    def clear(self):
+        if self.lock is None:
+            self._clear()
+            return
+        with self.lock as l:
+            self._clear()
+
+    def _draw_item(self, index):
+        if index == Menu.active:
+            self.display.set_color(config.MENU_ACTIVE_ITEM_COLOR,
+                                   config.MENU_ACTIVE_BG_COLOR)
+        else:
+            self.display.set_color(config.MENU_INACTIVE_ITEM_COLOR,
+                                   config.MENU_INACTIVE_BG_COLOR)
+        name, callback = self.menuitems[index]
+        self.display.set_font(config.MENU_FONT)
+        self.display.chars(name,
+                           config.MENU_START_X + config.MENU_ITEM_OFFSET,
+                           config.MENU_START_Y + config.MENU_ITEM_OFFSET + (index * config.MENU_ITEM_SPACING_Y))
+
+    def draw_item(self, index):
+        if self.lock is None:
+            self._draw_item(index)
+            return
+        with self.lock as l:
+            self._draw_item(index)
+
+    def draw_items(self):
+        for index in range(0, self.num_items):
+            self.draw_item(index)
+        gc.collect()
