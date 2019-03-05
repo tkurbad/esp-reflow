c65237769d909c8b84c7072ac62bb5c84eaf6f17
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2019-03-05 17:51:13 +0100

    Added comments

--- display/basic.py
+++ display/basic.py
@@ -1,14 +1,16 @@
-## ESP32 Reflow Oven
+# Basic Display Classes for ESP32 Micropython Driven SMD Reflow Oven
 #
-# Display Class that Does All the Work on the TFT
+# MIT license; Copyright (c) 2019 Torsten Kurbad
 
 from ili9341 import ILI9341
 
 import config
 
 class Display(ILI9341):
+    """ Basic Reflow Oven Display Class. """
 
     def __init__(self):
+        """ Initialize Display with Pre-Configured Parameters. """
         super().__init__(busid = config.DISPLAY_BUSID,
                          cs = config.DISPLAY_CS_PIN,
                          dc = config.DISPLAY_DC_PIN,
@@ -19,7 +21,7 @@ class Display(ILI9341):
         """ Erase Display, Set Up Status Bars, etc. """
         # Erase
         self.erase()
-        # Prepare top and bottom status bars
+        # Prepare Top and Bottom Status Bars
         self.set_color(config.DISPLAY_TOP_BAR_FG_COLOR,
                        config.DISPLAY_TOP_BAR_BG_COLOR)
         self.fill_rectangle(0,
@@ -37,19 +39,23 @@ class Display(ILI9341):
                             config.DISPLAY_WIDTH,
                             1,
                             color = config.DISPLAY_DELIM_COLOR)
+        # Top Status Bar
         self.set_font(config.DISPLAY_TOP_BAR_FONT)
         self.ipaddress_x = self.chars('IP ',
                                       config.DISPLAY_TOP_BAR_TEXT_X,
                                       config.DISPLAY_TOP_BAR_TEXT_Y)
+        # Heater Status Bar Label
         self.set_font(config.DISPLAY_LABEL_FONT)
         self.set_color(config.DISPLAY_LABEL_FG_COLOR,
                        config.DISPLAY_LABEL_BG_COLOR)
         self.chars(config.DISPLAY_HEATER_LABEL,
                    config.DISPLAY_LABEL_X,
                    config.DISPLAY_HEATER_LABEL_Y)
+        # Thermocouple Status Bar Label
         self.chars(config.DISPLAY_LOW_BAR_LABEL,
                    config.DISPLAY_LABEL_X,
                    config.DISPLAY_LOW_BAR_LABEL_Y)
+        # Thermocouple Names
         self.set_color(config.DISPLAY_THERMOCOUPLE_FG_COLOR,
                        config.DISPLAY_THERMOCOUPLE_BG_COLOR)
         self.set_font(config.DISPLAY_LOW_BAR_FONT)
@@ -73,6 +79,7 @@ class Display(ILI9341):
                                                 self.tc_x[config.THERMOCOUPLE_NAME4]
                                                 )
         self.tc_x[config.THERMOCOUPLE_NAME4] = self.tc_x[config.THERMOCOUPLE_NAME2]
+        # Heater Names
         self.set_color(config.DISPLAY_HEATER_FG_COLOR,
                        config.DISPLAY_HEATER_BG_COLOR)
         self.heater_x = dict()
@@ -85,6 +92,7 @@ class Display(ILI9341):
         self.prepared = True
 
     def show_ipaddress(self, ipaddress):
+        """ Display Current IP Address in Top Status Bar. """
         if not self.prepared:
             return
         self.set_color(config.DISPLAY_TOP_BAR_FG_COLOR,
@@ -95,6 +103,9 @@ class Display(ILI9341):
                    config.DISPLAY_TOP_BAR_TEXT_Y)
 
     def show_temperatures(self, temperatures):
+        """ Display Current Thermocouple Temperature Readings in Bottom
+            Status Bar.
+        """
         if not self.prepared:
             return
         if len(temperatures) != config.NUM_THERMOCOUPLES:
@@ -112,6 +123,9 @@ class Display(ILI9341):
                    config.DISPLAY_LOW_BAR_TEXT_Y[config.THERMOCOUPLE_NAME4])
 
     def show_heaters(self, heater_duty):
+        """ Display Currently Set Heater Duty Cycle Values in Heater
+            Status Bar.
+        """
         if not self.prepared:
             return
         if len(heater_duty) != config.NUM_HEATERS:
