46e60499a628e9a7bccc7fa14e0828f538131916
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2019-03-05 17:56:27 +0100

    Start implementing menu handling / Restructuring / Combine Threads / Add comments and copyright header

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Main Execution File for ESP32 Micropython Driven SMD Reflow Oven
+#
+# MIT license; Copyright (c) 2019 Torsten Kurbad
+
 import gc
 import _thread
 
@@ -9,19 +13,17 @@ import config
 
 from display.basic import Display
 from thermocouple.thermocouple import Thermocouple
-from reflow.device import Buzzer, Fan, HeaterBottom, HeaterTop, Light, RotaryEncoder
+from reflow.device import Buzzer, Fan, HeaterBottom, HeaterTop, Light
+from reflow.device import RotaryEncoder, SDCardHandler
 from reflow.menu import Menu
-#from sdcard import SDCard
-from reflow.sdcard import SDCard
 from wlan_sta import STA
 
 ##
-## Setup - 1st Part
+## Setup
 ##
 
 # SD Card Present?
 SD_CARD_MOUNTED = False
-sd = None
 
 # Initialize Display
 tft = Display()
@@ -47,56 +49,12 @@ heater_duty = {
     config.HEATER_NAME_TOP: 0.0,
     config.HEATER_NAME_BOTTOM: 0.0
 }
-##
-## Basic Threads
-##
-
 # Allocate a lock
 reflowLock = _thread.allocate_lock()
 
 # Get and Display IP Address
 ipaddress = STA().ipaddress
 tft.show_ipaddress(ipaddress)
-
-def thermocoupleReadThread(lock):
-    """ Read Thermocouples Every 500 ms. """
-    while True:
-        with lock as l:
-            thermocouples.read_temps()
-        sleep(0.5)
-
-def statusDisplayThread(lock):
-    """ Display Status, i.e. IP-Address and Temperatures on TFT. """
-    global ipaddress
-    while True:
-        with lock as l:
-            tft.show_temperatures(thermocouples.temp)
-            if ipaddress != STA().ipaddress:
-                ipaddress = STA().ipaddress
-                tft.show_ipaddress(ipaddress)
-        sleep(1)
-
-def heaterDisplayThread(lock):
-    """ Display PWM Duty Setting of Both Heaters. """
-    global heater_duty
-    while True:
-        heater_duty[config.HEATER_NAME_TOP] = heater_top.duty()
-        heater_duty[config.HEATER_NAME_BOTTOM] = heater_bottom.duty()
-        with lock as l:
-            tft.show_heaters(heater_duty)
-        sleep(1)
-
-# Start Reading Thermocouples (with Locking)
-_thread.start_new_thread(thermocoupleReadThread, (reflowLock, ))
-# Start Displaying Satus (with Locking)
-_thread.start_new_thread(statusDisplayThread, (reflowLock, ))
-
-# Run Garbage Collector
-gc.collect()
-
-##
-## Setup - 2nd Part
-##
 
 # Set Up Buzzer
 buzzer = Buzzer()
@@ -110,86 +68,104 @@ heater_bottom = HeaterBottom()
 # Set Up Fan
 fan = Fan()
 
-
-# Try to Mount SD Card
-try:
-    with reflowLock as l:
-        sd = SDCard(cs = config.SDCARD_CS)
-except OSError as e:
-    if 'no sd card' in e.args[0].lower():
-        print("Initialize regular watch for SD Card (via thread or timer)")
-
-if sd is not None:
-    try:
-        mount(sd, '/sd')
-        SD_CARD_MOUNTED = True
-    except OSError:
-        try:
-            with reflowLock as l:
-                sd.init_card_v1()
-            mount(sd, '/sd')
-            SD_CARD_MOUNTED = True
-        except OSError:
-            pass
-
-# Initialize Menu Display
-menuitems = (
-    ('hallo', None),
-    ('ballo', None),
-    ('popallo', None),
-    ('fan', None),
-)
-
 # Set Up Rotary Encoder
 rotary = RotaryEncoder()
 
-menu = Menu(menuitems, tft, rotary, lock = reflowLock)
-menu.draw_items()
+# Define Menu Items
+menuitems = (
+    ('hallo', None, None),
+    ('ballo', None, None),
+    ('popallo', None, None),
+    ('fan', None, None),
+)
 
+# Set Up Menu
+menu = Menu(menuitems, tft, rotary, lock = reflowLock)
+
+##
+## Basic Threads
+##
+def heatReadThread(lock):
+    """ Read Heater PWM Duty Settings and Thermocouple Values Every
+        500 ms.
+    """
+    global heater_duty
+    while True:
+        heater_duty[config.HEATER_NAME_TOP] = heater_top.duty()
+        heater_duty[config.HEATER_NAME_BOTTOM] = heater_bottom.duty()
+        with lock as l:
+            thermocouples.read_temps()
+        sleep_ms(500)
+
+def statusDisplayThread(lock):
+    """ Display Status, i.e. IP-Address and Temperatures on TFT. """
+    global ipaddress
+    while True:
+        with lock as l:
+            tft.show_heaters(heater_duty)
+            tft.show_temperatures(thermocouples.temp)
+            if ipaddress != STA().ipaddress:
+                ipaddress = STA().ipaddress
+                tft.show_ipaddress(ipaddress)
+        sleep(1)
+
+# Start Reading Heat Values (with Locking)
+_thread.start_new_thread(heatReadThread, (reflowLock, ))
+# Start Displaying Status (with Locking)
+_thread.start_new_thread(statusDisplayThread, (reflowLock, ))
+
+# Run Garbage Collector
+gc.collect()
+
+# Initialize and Try to Mount SD Card
+sdcard = SDCardHandler()
+SD_CARD_MOUNTED = sdcard.mount()
 
 ##
 ## Main Code
 ##
 
-# Start Displaying Heater Settings (with Locking)
-_thread.start_new_thread(heaterDisplayThread, (reflowLock, ))
+# Initial Menu Display
+menu.draw_items()
 
 # Play Jingle
 buzzer.jingle()
 
-with heater_bottom as hb, heater_top as ht, fan as f, light as li:
-    li.on()
-    hb.duty(100)
-    ht.duty(50)
-    f.duty(20)
-    sleep(20)
-    hb.duty(30)
-    ht.duty(100)
-    sleep(10)
-    hb.duty(10)
-    ht.duty(10)
-    sleep(10)
+# with heater_bottom as hb, heater_top as ht, fan as f, light as li:
+    # li.on()
+    # hb.duty(100)
+    # ht.duty(50)
+    # f.duty(20)
+    # sleep(20)
+    # hb.duty(30)
+    # ht.duty(100)
+    # sleep(10)
+    # hb.duty(10)
+    # ht.duty(10)
+    # sleep(10)
 
-#try:
-#    while True:
-#        sleep(1)
-#finally:
-#    heater_bottom.deinit()
-#    heater_top.deinit()
-#    fan.deinit()
-#    buzzer.deinit()
-lastval, lastbutton = rotary.value()
-while True:
-    val, butstate = rotary.value()
-    if butstate != lastbutton:
-        lastbutton = butstate
-        print('Button:', butstate)
-    if val != lastval:
-        lastval = val
-        print('Value:', val)
-    sleep_ms(50)
 
-heater_bottom.deinit()
-heater_top.deinit()
-fan.deinit()
-buzzer.deinit()
+###
+# IDEE:
+# Callback-Funktionen / IRQ-Handler fuer Rotary konfigurierbar machen
+# a la rotary.button_callback(callback)
+###
+
+##
+# TODO:
+# - SD-Karten Filebrowser
+# - Symbole Fan/Licht/SD
+
+try:
+    """ Handle Menu Input. """
+    menu.loop()
+
+finally:
+    """ Deinitialize all PWM and SPI Devices.
+        !!! Disable Heaters First to Prevent Fires !!!
+    """
+    heater_bottom.deinit()
+    heater_top.deinit()
+    fan.deinit()
+    buzzer.deinit()
+    sdcard.deinit()
