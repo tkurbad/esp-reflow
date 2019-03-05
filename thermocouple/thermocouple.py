6ac4e3540d995bff084106d83e5723a2406aeb2e
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2019-03-05 17:54:48 +0100

    Better parameter handling / Added comments and copyright header

--- thermocouple/thermocouple.py
+++ thermocouple/thermocouple.py
@@ -1,6 +1,7 @@
-## ESP32 Reflow Oven
+# Temperature Reading Classes for ESP32 Micropython Driven
+# SMD Reflow Oven
 #
-# Temperature Read Thread Definition
+# MIT license; Copyright (c) 2019 Torsten Kurbad
 
 import _thread
 from time import sleep
@@ -10,26 +11,40 @@ from max31855 import MAX31855
 import config
 
 class Thermocouple:
+    """ Class to Manage and Read Several MAX31855 Thermocouple
+        Amplifiers Connected to the Same Hardware SPI Bus, and their
+        Values.
+    """
+    _busid = None       # Default Hardware SPI Bus Id
+    _baudrate = None    # Default Baudrate
+    _tcs = dict()       # Dictionary Holding All Configured Thermocouples
+    _temps = dict()     # Dictionary Holding Last Temperature Readings
 
-    _busid = None
-    _baudrate = None
-    _tcs = dict()
-    _temps = dict()
-
-    def __init__(self, busid = config.THERMOCOUPLE_BUSID,
-        baudrate = config.THERMOCOUPLE_BAUDRATE):
-        if Thermocouple._busid is None:
+    def __init__(self, busid = None, baudrate = None):
+        """ Initialize the Hardware SPI Bus for Use with MAX31855
+            Thermocouple Amplifiers.
+        """
+        if busid is not None:
+            if ((Thermocouple._busid is not None)
+                and (Thermocouple._busid != busid)):
+                raise RuntimeError('Thermocouple busid already initialized with %d'
+                                    % Thermocouple._busid)
             Thermocouple._busid = busid
-        if Thermocouple._busid != busid:
-            raise RuntimeError('Thermocouple busid already initialized with %d'
-                                % Thermocouple._busid)
-        if Thermocouple._baudrate is None:
+        if Thermocouple._busid is None:
+            Thermocouple._busid = config.THERMOCOUPLE_BUSID
+        if baudrate is not None:
+            if ((Thermocouple._baudrate is not None)
+                and (Thermocouple._baudrate != baudrate)):
+                raise RuntimeError('Thermocouple baudrate already initialized with %d'
+                                   % Thermocouple._baudrate)
             Thermocouple._baudrate = baudrate
-        if Thermocouple._baudrate != baudrate:
-            raise RuntimeError('Thermocouple baudrate already initialized with %d'
-                                % Thermocouple._baudrate)
+        if Thermocouple._baudrate is None:
+            Thermocouple._baudrate = config.THERMOCOUPLE_BAUDRATE
 
     def add_tc(self, name, cs):
+        """ Add and Initialize a Thermocouple with Index Name 'name' and
+            Chip Select Pin Number 'cs'.
+        """
         if name is None:
             raise RuntimeError('Thermocouple must have a name')
         if name in Thermocouple._tcs.keys():
@@ -45,6 +60,8 @@ class Thermocouple:
         Thermocouple._temps[name] = (0.0, 0.0)
 
     def remove_tc(self, name):
+        """ Remove the Thermocouple with Index Name 'name' from the
+            List of Active Thermocouples. """
         if name is None:
             raise RuntimeError('Thermocouple must have a name')
         if name not in self._tcs.keys():
@@ -53,9 +70,14 @@ class Thermocouple:
         del(Thermocouple._temps[name])
 
     def read_temps(self):
+        """ Read Temperatures from All Configured Thermocouples and
+            Store them in the Class Variable _temps.
+        """
         for (name, tc) in Thermocouple._tcs.items():
             Thermocouple._temps[name] = tc.read()
 
     @property
     def temp(self):
+        """ Return the Last Temperature Readings of All Thermocouples.
+        """
         return Thermocouple._temps
