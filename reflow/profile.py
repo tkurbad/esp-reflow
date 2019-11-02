6b228228d8be0f60da5fab03974a089c0cda442b
Torsten Kurbad <github@tk-webart.de>
2019-11-02 21:51:36 +0100

    Redesign Current Profile Save Method

--- reflow/profile.py
+++ reflow/profile.py
@@ -5,7 +5,6 @@
 
 from gc import collect
 
-from ujson import dump, load
 from uos import ilistdir, stat
 from utime import sleep_ms
 
@@ -18,7 +17,7 @@ from config import MENU_INACTIVE_ITEM_COLOR, MENU_INACTIVE_BG_COLOR
 from reflow.menu import BaseMenu, MainMenu
 
 
-class ReflowProfile:
+class ReflowProfile():
     """ Class for Holding Data of a Single Reflow Profile. """
     def __init__(self, name = None, entries = []):
         self._name = name
@@ -38,12 +37,15 @@ class ReflowProfile:
         return self._entries
 
     def append_entry(self, line):
+        # Empty line?
+        if not line.strip():
+            return
         linesplit = line.split(',')
         if len(linesplit) != 3:
             raise ValueError('Profile syntax error in line {0:d}'.format(self.line_num))
 
         try:
-            intline = [int(i.strip()) for i in line]
+            intline = [int(i.strip()) for i in linesplit]
         except ValueError:
             raise ValueError('Non numeric value in profile line {0:d}'.format(self.line_num))
 
@@ -67,7 +69,7 @@ class ProfileControl:
         self.profiles           = []
         self.current_profile    = current_profile
         self.buf                = bytearray(250)
-        self.err                = None
+        self._err                = None
 
     def listProfiles(self, extension = None):
         if extension is None:
@@ -78,7 +80,7 @@ class ProfileControl:
             return len(self.profiles)
         except OSError as e:
             if 'sd card not mounted' in '{}'.format(e.args[0].lower()):
-                self.err = e.args[0]
+                self._err = e.args[0]
                 self.profiles = []
             else:
                 raise (e)
@@ -86,7 +88,7 @@ class ProfileControl:
         self.profiles = sorted(self.profiles)
         return len(self.profiles)
 
-    def readProfile(self, profile_path):
+    def readProfile(self, profile_path, save = True):
         profile_len = 0
         with open(profile_path) as profile:
             profile_len = profile.readinto(self.buf)
@@ -97,19 +99,18 @@ class ProfileControl:
         temp_profile.name = profile_lines[0]
 
         # Profile Entries (Setpoint, Soaktime, Overshoot_Prevention_Strength)
-        index = 0
-        profile_lines_num = len(profile_lines)
-        while index < profile_lines_num:
-            index += 1
+        for profile_line in profile_lines[1:]:
             try:
-                temp_profile.append_entry(profile_lines[index])
+                temp_profile.append_entry(profile_line)
             except ValueError as e:
-                self.err = e.args[0]
+                self._err = e.args[0]
                 self.current_profile = None
                 return
 
         # Success!
         self.current_profile = temp_profile
+        if save:
+            self.setCurrentProfileAsDefault()
 
     def getDefaultProfile(self, default_filename = None):
         if default_filename is None:
@@ -118,19 +119,29 @@ class ProfileControl:
             stat(default_filename)
         except OSError as e:
             if e.args[0] == 2:
-                self.err = 'Default profile not found'
+                self._err = 'Default profile not found'
                 return None
             else:
                 raise(e)
 
-        with open(default_filename) as def_profile:
-            self.current_profile = load(def_profile)
+        self.readProfile(default_filename, save = False)
 
     def setCurrentProfileAsDefault(self, default_filename = None):
+        if self.current_profile is None or len(self.current_profile) == 0:
+            return
+
         if default_filename is None:
             default_filename = DEFAULT_PROFILE_FILE
         with open(default_filename, 'w') as def_profile:
-            dump(self.current_profile, def_profile)
+            def_profile.write('{}\n'.format(self.current_profile.name))
+            for (sp, st, op) in self.current_profile.entries:
+                def_profile.write('{},{},{}\n'.format(sp, st, op))
+
+    @property
+    def err(self):
+        err = self._err
+        self._err = None
+        return err
 
 
 class ProfileLoaderMenu(BaseMenu):
@@ -150,11 +161,6 @@ class ProfileLoaderMenu(BaseMenu):
             entries, display, rotary, button_up, button_down,
             button_left, button_right, lock)
 
-    def read_profile(self, profile_path):
-        self.profile_control.readProfile(profile_path)
-        self.profile_control.setCurrentProfileAsDefault()
-        self.go_back()
-
     def draw_items(self):
         """ (Re-)Draw All Profiles Currently on SD Card, Highlighting
             the Currently Active One.
@@ -162,13 +168,13 @@ class ProfileLoaderMenu(BaseMenu):
         if ProfileLoaderMenu.paused:
             return
 
-        entries = [['< Back to Main Menu', self.go_back, None]]
+        entries = [['< Back to Main Menu', self.cb_go_back, None]]
         mountpoint = self.mountpoint
 
         profile_count = self.profile_control.listProfiles()
         for profile in self.profile_control.profiles:
             entries.append([profile,
-                            self.read_profile,
+                            self.cb_read_profile,
                             '{}/{}'.format(mountpoint, profile)
                             ])
 
@@ -223,7 +229,11 @@ class ProfileLoaderMenu(BaseMenu):
         sleep_ms(50)
         self.draw_item(index)
 
-    def go_back(self):
+    def cb_go_back(self):
         ProfileLoaderMenu.paused = True
         MainMenu.was_paused = True
         MainMenu.paused = False
+
+    def cb_read_profile(self, profile_path):
+        self.profile_control.readProfile(profile_path)
+        self.cb_go_back()
